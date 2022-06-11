62b0010f550a5ca3cf23c058731642e0e0debd0f
leonkae <leon29kae@gmail.com>
2022-06-11 08:52:45 +0300

    migrations done initital.py signup templates added

--- critique/admin.py
+++ critique/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Ratings, Reviews, Project
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Ratings)
+admin.site.register(Reviews)
+admin.site.register(Project)
+
--- critique/views.py
+++ critique/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 
 def home(request):
     return render(request,'critiques/home.html')
+
+def signup(request):
+    ''' signup view '''
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form= CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                created_user = form.save()
+                Profile.objects.get_or_create(user=created_user)
+                messages.success(request, 'Account for' + username + 'created succesfully')
+                return redirect('home')   
+             
+    context ={'form': form}
+    return render(request, 'critiques/signup.html', context)
