9f421e10bcccd97f7ba0c0d4f6cc6c7dabb0e840
leonkae <leon29kae@gmail.com>
2022-06-14 15:36:09 +0300

    tests

--- critique/tests.py
+++ critique/tests.py
@@ -1,9 +1,11 @@
 
 from django.test import TestCase
-from  .models  import *
+from  .models  import Profile, Project, Reviews
 # Create your tests here.
 
 class ProfileTestClass(TestCase):
+    '''profile test class'''
+    
     def setUp(self):
         self.profile = Profile(pro_picture ='image.jpg', bio = 'text', email = 'johndoe@gmail.com')
         
@@ -15,4 +17,29 @@ class ProfileTestClass(TestCase):
         profile = Profile.objects.all()
         self.assertTrue(len(profile)>0)   
 
+class ProjectTestClass(TestCase):
+    '''project test class'''
+       
         
+    def setUp(self):
+        self.project = Project(image = 'image.jpg', projectname = 'project', description = 'text') 
+            
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Project))  
+    
+    def tearDown(self):
+        Project.objects.all().delete      
+    
+class ReviewsTestClass(TestCase):
+    '''reviews test class'''
+    
+    def setup(self):
+        self.reviews = Reviews(text='text')
+        self.reviews.save()
+   
+    def test_instance(self):
+        self.assertTrue(isinstance(self.reviews,Reviews))       
+            
+    def tearDown(self):
+        Reviews.objects.all().delete
+            
