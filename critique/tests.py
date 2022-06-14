4eb0b45aaac44e0239f9a8ef8a72da26b7bb87de
leonkae <leon29kae@gmail.com>
2022-06-14 14:58:41 +0300

    tests OK

--- critique/tests.py
+++ critique/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
 
+from django.test import TestCase
+from  .models  import *
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile = Profile(pro_picture ='image.jpg', bio = 'text', email = 'johndoe@gmail.com')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+        
+        
+    
